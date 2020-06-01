5d71e87a9787ef4deb36d6b1fe02bc768d4a40a9
Kyle Burke <kburke@solstice.com>
2020-06-01 15:03:48 -0500

    Styling for login page

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,18 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
+import styles from './Login.module.css';
 
 function Login() {
   return (
-    <div>
-        <h1>Welcome!</h1>
-        <div className='ui input'>
-          <input id='usernameField' type='text' placeholder='Your name' />
-          <input id='passwordField' type='password' placeholder='Your password' />
+    <div className={styles.container}>
+        <h1 className={styles.heading}>Welcome!</h1>
+        <div className={styles.inputFieldContainer}>
+          <div className={styles.usernameField + ' ui input'}>
+            <input id='usernameField' type='text' placeholder='Your name' />
+          </div>
+          <div className={styles.passwordField + ' ui input'}>
+            <input id='passwordField' type='password' placeholder='Your password' />
+          </div>
         </div>
-        <br />
         
         <Link to="/posts">
-          <button id='loginButton' className='ui primary button'>Log in</button>
+          <button id='loginButton' className={styles.button + ' ui primary button'}>Log in</button>
         </Link>
     </div>
   );
